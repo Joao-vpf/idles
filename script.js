c74d1793c3ba84e2d838bc583acfe3761585e04e
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-11-18 01:10:45 -0300

    Update script.js
    mudei muitas coisas, organizei, arrumei cores

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let vida = 7;
+const x = 'MANGA';
+var last_block = -1; 
 
 document.addEventListener('keydown', (event) => {
-    const block = event.target;
+    block = event.target;
+    if (last_block !== -1 && (event.key === 'Backspace' || event.key ===  'ArrowLeft' || event.key === 'Enter'))
+    {
+        block = last_block;
+        last_block = -1;
+    }
+
+
     if (!block.classList.contains('input-block')) {
         return;
     }
@@ -9,104 +18,163 @@ document.addEventListener('keydown', (event) => {
     const blocks = Array.from(block.parentNode.children);
     const index = blocks.indexOf(block);
     const key = event.key;
-	
-	if (key.length === 1 && !key.match(/[a-z]/i)) {
-        event.preventDefault();
-        return;
-    }
 
-
-    if (key === 'Enter') 
-    {
-        const word = blocks.map(block => block.textContent).join('');
-        const x = 'MANGA'
-        
-        if (word.length === 5)
-        {
-			
-            if (word === x) 
-            {//Animação e finalização dos blocos ao acertar a palavra
-				blocks.forEach((block, index) => {
-					setTimeout(() => { 
-						block.contentEditable = false; 
-						block.style.backgroundColor = '#00a471';
-						block.classList.add('onda');
-					}, index * 40); 
-				});
-			}
-            else
-            {//errou a palavra
-
-				vida-=1;
-                //criar um novo container
-                const newContainer = document.createElement('div');
-                newContainer.className = 'blocks-container';
-                for(let i=1; i<=5; i++){
-                    const newBlock = document.createElement('div');
-                    newBlock.className = 'input-block';
-                    newBlock.id = `${i}`;
-                    newBlock.contentEditable = true;
-                    newContainer.appendChild(newBlock);
-                }
-                
-                //pintar no bloco novas cores
-				blocks.forEach(block => {
-					block.contentEditable = false;
-					if(block.textContent === x[parseInt(block.id)-1]) 
-						block.style.backgroundColor = '#00a471';
-				});
-				
-				blocks.forEach(block => {
-					if(x.includes(block.textContent)) 
-                    {
-                        const countInX = Array.from(x).filter(letter => letter === block.textContent).length;
-                        const countInBlocks = Array.from(blocks).filter(b => b.textContent === block.textContent && (window.getComputedStyle(b).backgroundColor === '#f9f494' || window.getComputedStyle(b).backgroundColor === '#00a471')).length;
-                        
-						if(countInBlocks < countInX && window.getComputedStyle(block).backgroundColor === 'rgba(0, 95, 107, 0.3)') 
-                        {
-							block.classList.add('yshake');
-							block.style.backgroundColor = '#f9f494';
-						}
-					}
-					if(window.getComputedStyle(block).backgroundColor === 'rgba(0, 95, 107, 0.3)')
-					{
-						block.classList.add('rshake');
-						block.style.backgroundColor = '#e87f7f'; 
-					}
-				});
-				
-				if (vida > 0)
-				{
-					document.body.appendChild(newContainer);
-					newContainer.children[0].focus();
-				}
+    switch (key) {
+        case 'Enter':
+            checkWord(blocks);
+            break;
+        case 'Backspace':
+            if (blocks[index].textContent === "" && index > 0)
+            {
+                blocks[index-1].focus();
+                blocks[index-1].textContent = "";
             }
-        }
+            else
+            {
+                blocks[index].focus();
+                blocks[index].textContent = "";
+            }
+            break;
+        case 'ArrowLeft':
+            if (index > 0) {
+                blocks[index - 1].focus();
+            }
+            break;
+        case 'ArrowRight':
+            if (index < blocks.length - 1) {
+                blocks[index + 1].focus();
+            }
+            break;
+        default:
+            if (key.length === 1 && key.match(/[a-z]/i)) {
+                blocks[index].textContent = key.toUpperCase();
+                blocks[index].classList.add('grow'); 
+                if (index < blocks.length - 1) {
+                    blocks[index + 1].focus();
+                } 
+                else 
+                {
+                    last_block = blocks[index];
+                    blocks[index].blur();
+                }
+                blocks[index].addEventListener('animationend', function() {
+                    this.classList.remove('grow');
+                });
+                
+            }
     }
-
-    if (key.length === 1 && key.match(/\S/)) 
-    {
-        blocks[index].textContent = key.toUpperCase();
-        if (index < blocks.length - 1) 
-            blocks[index + 1].focus();
-    } 
-    if (key === 'Backspace') 
-    {
-        blocks[index].textContent = "";
-        if (index > 0) 
-            blocks[index - 1].focus();
-    }
-
-    if ( key === 'ArrowLeft')
-        if (index > 0) 
-            blocks[index - 1].focus();
-    if ( key === 'ArrowRight')
-        if (index < blocks.length - 1) 
-            blocks[index + 1].focus();
+    
 
     event.preventDefault();
 });
 
+function checkWord(blocks) {
+    const word = blocks.map(block => block.textContent).join('');
+    if (word.length === 5) {
+        if (word === x) {
+            animateBlocks(blocks);
+        } else {
+            removeheart();
+            vida -= 1;
+            handleWrongWord(blocks);
+        }
+    }
+}
+
+function animateBlocks(blocks) {
+    blocks.forEach((block, index) => {
+        setTimeout(() => { 
+            block.contentEditable = false; 
+            block.style.transition = 'background-color 0.1s ease';
+            block.style.backgroundColor = 'rgb(0, 164, 113)';
+            block.classList.add('onda');
+        }, index * 40); 
+    });
+}
+
+function removeheart()
+{
+    var heart = document.getElementById('heart' + vida);
+    heart.parentNode.removeChild(heart);
+}
+
+function handleWrongWord(blocks) {
+    blocks.forEach(block => {
+        block.contentEditable = false;
+        if(block.textContent === x[parseInt(block.id)-1]) 
+        {   
+            block.style.backgroundColor = 'rgb(0, 164, 113)';
+        }
+    });
+    
+    blocks.forEach(block => {
+        if(x.includes(block.textContent)) 
+        {
+            const countInX = Array.from(x).filter(letter => letter === block.textContent).length;
+            const countInBlocks = Array.from(blocks).filter(b => b.textContent === block.textContent && (window.getComputedStyle(b).backgroundColor === 'rgb(249, 244, 148)' || window.getComputedStyle(b).backgroundColor === 'rgb(0, 164, 113)')).length;
+           
+
+            if(countInBlocks < countInX && window.getComputedStyle(block).backgroundColor === 'rgba(0, 95, 107, 0.3)') 
+            {
+                block.classList.add('yshake');
+                block.style.transition = 'background-color 0.3s ease';
+                block.style.backgroundColor = 'rgb(249, 244, 148)';
+                console.log(countInX+" "+ countInBlocks+" " +window.getComputedStyle(block).backgroundColor )
+            }
+            else
+            {
+                if(window.getComputedStyle(block).backgroundColor === 'rgba(0, 95, 107, 0.3)')
+                {
+                    block.classList.add('rshake');
+                    block.style.transition = 'background-color 0.5s ease';
+                    block.style.backgroundColor = 'rgb(232, 127, 127)'; 
+                }
+            }
+        }
+        else
+        {
+            if(window.getComputedStyle(block).backgroundColor === 'rgba(0, 95, 107, 0.3)')
+            {
+                block.classList.add('rshake');
+                block.style.transition = 'background-color 0.5s ease';
+                block.style.backgroundColor = 'rgb(232, 127, 127)'; 
+            }
+        }
+    });
+
+    if (vida > 0) {
+        createNewContainer();
+    }
+    else
+    {
+        customAlert("A palavra era: " + x)
+    }
+}
+function customAlert(msg) {
+    // Cria um novo elemento div
+    var alertBox = document.getElementById('alert1');
+    
+    alertBox.style.opacity = "1"
+
+    // Define o texto do alerta
+    alertBox.textContent = msg;
+
+}
+
+
+function createNewContainer() {
+    const newContainer = document.createElement('div');
+    newContainer.className = 'blocks-container';
+    for(let i=1; i<=5; i++){
+        const newBlock = document.createElement('div');
+        newBlock.className = 'input-block';
+        newBlock.id = `${i}`;
+        newBlock.contentEditable = true;
+        newContainer.appendChild(newBlock);
+    }
+    document.body.appendChild(newContainer);
+    newContainer.children[0].focus();
+}
 
 document.addEventListener('click', (event) => {
     const block = event.target;
